8f69b6ec86a9a5a50637feff53e07e0ac8ff1d18
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-12 19:22:17 +0800

    use atomic design with prefix false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,11 @@ export default defineNuxtConfig({
             },
           },
         },
-    }
+    },
+    components: [
+      {
+        path: '~/components',
+        pathPrefix: false,
+      },
+    ],
 })
