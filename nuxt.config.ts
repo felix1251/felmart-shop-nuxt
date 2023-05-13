d8f18f4fe77c6b5178b78e3ba6861b680c89a13b
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-13 21:22:17 +0800

    arrange nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,20 +7,13 @@ export default defineNuxtConfig({
             viewport: 'width=device-width, initial-scale=1',
         }
     },
-    css: ['@/assets/css/main.css'],
-    components: [
-        {
-            path: '~/components',
-            pathPrefix: false,
-        },
-    ],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
     },
-    modules: [
-        '@formkit/nuxt',
-    ],
+    components: [{ path: '~/components', pathPrefix: false }],
+    css: ['@/assets/css/main.css'],
+    modules: ['@formkit/nuxt'],
 })
