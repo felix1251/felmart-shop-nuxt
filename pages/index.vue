a80c9aeeb4800a5acfbaf8c6ef4540828c95abb0
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-13 20:02:01 +0800

    ..

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
     <FormKit type="text" label="Your username" value="calypso" help="Pick a username people will remember!" />
     <FormKit type="button" label="Checkout my label" />
-    <div class="text-red-400">felix</div>
 </template>
