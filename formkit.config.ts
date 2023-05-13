416d90f05053f00b7bed94a25afc9e4b585de47f
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-13 21:15:59 +0800

    add nuxt loading indicator

--- formkit.config.ts
+++ formkit.config.ts
@@ -5,9 +5,9 @@ export default <DefaultConfigOptions>{
     config: {
         classes: generateClasses({
             text: {
-                label: 'block mb-1 font-medium text-sm',
-                inner: 'max-w-md border border-gray-400 rounded-lg mb-1 overflow-hidden focus-within:border-blue-500',
-                input: 'w-full h-10 px-3 border-none text-base text-gray-700 placeholder-gray-400',
+                label: 'block mb-1 font-medium text-sm text-gray-900',
+                // inner: 'max-w-md mb-1 overflow-hidden',
+                input: 'w-full h-10 px-3 border rounded-lg border-gray-400 text-base text-gray-700 placeholder-gray-400 focus-within:outline-blue-500',
                 help: 'text-xs text-gray-500',
                 messages: 'list-none p-0 mt-1 mb-0',
                 message: 'text-red-500 mb-1 text-xs',
