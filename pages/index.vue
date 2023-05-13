5cac9021504c153f8191f4bc6324afe67e8a92a2
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-13 19:59:48 +0800

    custom button

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        <FormKit type="text" label="Your username" value="calypso" help="Pick a username people will remember!" />
-        <div class="text-red-400">felix</div>
-    </div>
+    <FormKit type="text" label="Your username" value="calypso" help="Pick a username people will remember!" />
+    <FormKit type="button" label="Checkout my label" />
+    <div class="text-red-400">felix</div>
 </template>
