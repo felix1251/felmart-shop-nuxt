0794e6f0e7455673733b751c4356a27a00cd8952
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-13 19:33:25 +0800

    fix formkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-      head: {
-        title: "Felmart PH",
-        charset: 'utf-8',
-        viewport: 'width=device-width, initial-scale=1',
-      }
-    },
-    css: [
-      '@/assets/css/index.css',
-    ],
-    components: [
-      {
-        path: '~/components',
-        pathPrefix: false,
-      },
-    ],
-    build: {
-      postcss: {
-        postcssOptions: {
-          plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-          },
-        },
-      },
+  app: {
+    head: {
+      title: "Felmart PH",
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
   },
+  css: ['@/assets/css/main.css'],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@formkit/nuxt',
+  ],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,40 @@
+import formkit from '@formkit/themes/tailwindcss';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
+  // corePlugins: {
+  //   preflight: false
+  // },
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
+    "./formkit.config.ts"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Inter"',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
   },
-  plugins: [],
+  plugins: [formkit]
 }
