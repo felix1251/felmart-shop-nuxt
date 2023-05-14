8ae225be044d03b679d82f6c1b6d79fed389b966
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-14 13:13:27 +0800

    logo text as component

---
+++ components/atoms/LogoText.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-4xl font-bold lowercase text-black">
+        felmart.
+    </div>
+</template>
--- components/molecules/Header.vue
+++ components/molecules/Header.vue
@@ -1,9 +1,7 @@
 <template>
     <nav class="border-b h-20 shadow-sm bg-white grid place-items-center">
         <div class="container mx-auto px-4">
-            <div class="text-4xl font-bold lowercase text-black">
-                felmart.
-            </div>
+            <LogoText/>
         </div>
     </nav>
 </template>
