58c1b50ab3a161d7b29580eea4c4b3fc491ea812
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-14 13:44:41 +0800

    logo as livk

--- formkit.config.ts
+++ formkit.config.ts
@@ -7,7 +7,7 @@ export default <DefaultConfigOptions>{
             text: {
                 label: 'block mb-1 font-medium text-sm text-secondary',
                 // inner: 'max-w-md mb-1 overflow-hidden',
-                input: 'w-full h-10 px-3 border font-normal rounded-lg border-gray-400 text-gray-700 placeholder-gray-400 focus-within:outline-primary',
+                input: 'w-full px-3 py-2.5 border font-normal rounded-lg border-gray-400 text-gray-700 placeholder-gray-400 focus-within:outline-primary',
                 help: 'text-xs text-gray-500 mt-1',
                 messages: 'list-none p-0 mt-1 mb-0',
                 message: 'text-red-500 mb-1 text-xs',
