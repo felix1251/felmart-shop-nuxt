a6be7e70b8b6236f39ca2c3399b2a44b152d0445
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-14 13:31:27 +0800

    nav w-full

--- components/molecules/Header.vue
+++ components/molecules/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="border-b h-20 shadow-sm bg-white grid place-items-center">
-        <div class="default-screen">
+        <div class="default-screen w-full">
             <LogoText/>
         </div>
     </nav>
