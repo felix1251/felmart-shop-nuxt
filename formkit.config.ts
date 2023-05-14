ce2a4812bdb90ee29c4b538c1dbb5a3fc57d5049
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-14 13:56:08 +0800

    change text input css

--- formkit.config.ts
+++ formkit.config.ts
@@ -5,7 +5,7 @@ export default <DefaultConfigOptions>{
     config: {
         classes: generateClasses({
             text: {
-                label: 'block mb-1 font-medium text-sm text-secondary',
+                label: 'block mb-1 text-secondary',
                 // inner: 'max-w-md mb-1 overflow-hidden',
                 input: 'w-full px-3 py-2.5 border font-normal rounded-lg border-gray-400 text-gray-700 placeholder-gray-400 focus-within:outline-primary',
                 help: 'text-xs text-gray-500 mt-1',
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <FormKit type="form" :actions="false">
-        <FormKit type="text" label="Email address" placeholder="Email"/>
+        <FormKit type="text" label="Email Address" placeholder="Email"/>
         <FormKit type="text" label="Password" placeholder="Password" />
     </FormKit>
 </template>
