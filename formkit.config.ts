7cea37722c54ae849a9cc8dd5e6fb8cfb6c17c58
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-16 13:14:26 +0800

    remove inner css text input

--- formkit.config.ts
+++ formkit.config.ts
@@ -6,7 +6,6 @@ export default <DefaultConfigOptions>{
         classes: generateClasses({
             text: {
                 label: 'block mb-1 text-secondary font-medium',
-                // inner: 'max-w-md mb-1 overflow-hidden',
                 input: 'w-full px-3 py-2.5 border font-normal rounded-lg border-gray-400 text-gray-800 placeholder-gray-400 focus-within:outline-primary',
                 help: 'text-xs text-gray-500 mt-1',
                 messages: 'list-none p-0 mt-1 mb-0',
