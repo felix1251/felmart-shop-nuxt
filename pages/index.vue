36f2d19e14fa6636a06d5f8c73d02829e8fc3022
felix.1251 <felix.abacajen.mgx@gmail.com>
2023-05-17 22:18:53 +0800

    use composables constant theme config

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,14 @@
     <FormKit type="form" :actions="false">
         <div class="space-y-4">
             <div class="grid grid-cols-2 gap-4">
-                <FormKit type="text" label="Email Address" placeholder="Email"/>
+                <FormKit type="text" label="Email Address" placeholder="Email" />
                 <FormKit type="text" label="Password" placeholder="Password" />
             </div>
             <FormKit type="button" label="Button" />
+            {{ foo }}
         </div>
     </FormKit>
 </template>
+
+<script lang="ts" setup>
+</script>
