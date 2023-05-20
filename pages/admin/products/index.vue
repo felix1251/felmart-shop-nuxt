57f46e5e589ed50cb17eb5d7d1ad7050f458ca1a
felix1251 <domhack123@gmail.com>
2023-05-20 15:06:02 +0800

    set admin layout

--- pages/admin/products/index.vue
+++ pages/admin/products/index.vue
@@ -3,3 +3,9 @@
         products
     </div>
 </template>
+
+<script lang="ts" setup>
+definePageMeta({
+    layout: "admin",
+});
+</script>
--- pages/admin/sales/index.vue
+++ pages/admin/sales/index.vue
@@ -3,3 +3,9 @@
         sales
     </div>
 </template>
+
+<script lang="ts" setup>
+definePageMeta({
+    layout: "admin",
+});
+</script>
