b9b595cb4967c8327e4dd492853160e225774b7e
felix1251 <domhack123@gmail.com>
2023-05-21 00:50:03 +0800

    sidebar links styles

--- app.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'vue-unicons/dist/icons';
--- composables/constants/sidebar.ts
+++ composables/constants/sidebar.ts
@@ -1,3 +1,6 @@
-const sidebar = [
-    {name: "", icon: "", link: ""}
-]
+export const adminLinksConstant = [
+    { name: "Dashboard", icon: "material-symbols:home-outline", link: "/admin"},
+    { name: "Products", icon: "ph:bag-bold", link: "/admin/products"},
+    { name: "Categories", icon: "ic:baseline-list", link: "/admin/category"},
+    { name: "Sales", icon: "ph:money-bold", link: "/admin/sales"},
+];
