f48d3778f4fe5127be0c79e05008781b74768300
“felix1251” <domhack123@gmail.com>
2023-05-23 15:10:04 +0800

    not found page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col h-screen items-center justify-center">
+        <div class="relative grid place-items-center">
+            <h1 class="text-[15rem] font-semibold text-primary/[.9]">404</h1>
+            <div
+                class="text-secondary absolute top-[50%] text-center text-5xl font-semibold"
+            >
+                Page Not Found
+            </div>
+        </div>
+        <a
+            class="bg-primary hover:bg-primary/[.9] text-white px-3 py-2 rounded-lg"
+            href="/"
+            >Go to main page</a
+        >
+    </div>
+</template>
