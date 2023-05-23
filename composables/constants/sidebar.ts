bdc17ebd7ac83f2caded9663754d4df3c1d76c17
felix1251 <domhack123@gmail.com>
2023-05-23 21:49:28 +0800

    remove sidebar composable, not used

--- composables/constants/sidebar.ts
+++
@@ -1,6 +0,0 @@
-export const adminSidebarConstant = [
-    { name: "Dashboard", icon: "material-symbols:home-outline", link: "/admin"},
-    { name: "Products", icon: "ph:bag-bold", link: "/admin/products"},
-    { name: "Categories", icon: "ic:baseline-list", link: "/admin/category"},
-    { name: "Sales", icon: "ph:money-bold", link: "/admin/sales"},
-];
