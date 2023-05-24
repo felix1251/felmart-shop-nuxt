c293acdee00afffbd1bd9146b80f3e7b2757bea1
“felix1251” <domhack123@gmail.com>
2023-05-24 09:18:42 +0800

    merge

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,27 @@
 <template>
-    <FormKit type="form" :actions="false">
+    <FormKit
+        type="form"
+        :actions="false"
+    >
         <div class="space-y-4">
             <div class="grid grid-cols-2 gap-4">
-                <FormKit type="text" label="Email Address" placeholder="Email" />
-                <FormKit type="text" label="Password" placeholder="Password" />
+                <FormKit
+                    type="text"
+                    label="Email Address"
+                    placeholder="Email"
+                />
+                <FormKit
+                    type="text"
+                    label="Password"
+                    placeholder="Password"
+                />
             </div>
-            <FormKit type="button" label="Button" />
+            <FormKit
+                type="button"
+                label="Button"
+            />
         </div>
     </FormKit>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
--- pages/vendor/index.vue
+++ pages/vendor/index.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>Content</div>
+    <div>
+        <FormKit
+            @click="click"
+            type="button"
+            label="Button"
+        />
+    </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({
     layout: "vendor",
 });
+const click = () => {
+    $toast.show("this is a test");
+};
 </script>
